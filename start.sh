e24bd71908f2cba23baf798dd671c62bf33d777b
Tyson <tys876@proton.me>
2025-03-28 04:06:34 +0000

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Start the VNC server:
+tightvncserver :1 -geometry 1280x1024 -depth 24
+
+# Start websockify to serve noVNC:
+websockify -D --web /usr/share/novnc 6080 localhost:5901
